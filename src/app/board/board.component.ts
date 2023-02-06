1c92a6c293c329614a687c2c866104833a82f810
Erwan <erwan.ngma@gmail.com>
2023-02-06 14:43:46 +0100

    adding modal

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -4,18 +4,28 @@ import { Card } from '../models/card.model';
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.scss']
+  styleUrls: ['./board.component.scss', '../card-box/card-box.component.scss']
 })
 export class BoardComponent {
   tasks!: number[];
   card!: Card[];
+  displayed!: string;
+
+  closeCard() {
+    this.displayed = 'display-none';
+  }
+  openCard() {
+    this.displayed = 'display-block';
+  }
 
   ngOnInit() {
     this.tasks = [];
+    this.closeCard()
   }
 
   addCard() {
     this.tasks.push(0)
   }
+
 
 }
